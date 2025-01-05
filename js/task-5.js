26970d5c09493d62ff255b58c96eb4a0dbbd6168
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-01-05 18:05:13 +0200

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector('body');
+const changeColor = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+changeColor.addEventListener('click', () => {
+  const colorBack = getRandomHexColor();
+  color.textContent = colorBack;
+  bodyColor.style.backgroundColor = colorBack;
+});
